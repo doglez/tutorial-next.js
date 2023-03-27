99f8dc01f3cda7fd125fd58a525012eb80fa4036
doglez <danilo.j.gonzalez@gmail.com>
2023-03-26 19:44:22 -0600

    use layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,14 +7,9 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
     return (
-        <main className="bg-gray-100 min-h-screen w-screen">
-            <main className="max-w-screen-2xl m-auto bg-white">
-                <NavBar />
-                <main>
-                    <Header />
-                    <RestaurantCard />
-                </main>
-            </main>
+        <main>
+            <Header />
+            <RestaurantCard />
         </main>
     );
 }
